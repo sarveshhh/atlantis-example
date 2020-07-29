0a1043c5770284e8e3875b47c2ce2bca548ef954
Sarvesh Gupta <sarveshhh@gmail.com>
2020-07-29 18:03:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,9 @@
-resource "null_resource" "test" {
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["22.22.22.22/0"]
+  }
 }
